f4e95ae4475bf0b167fcaf12f284d076ed02ece9
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-02-05 09:21:15 -0300

    organizing project in MVVM structure

---
+++ CatFacts/CatFacts/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  CatFacts
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 05/02/21.
+//
+
+import Foundation
---
+++ CatFacts/CatFacts/View/Facts/FactsView.swift
@@ -0,0 +1,8 @@
+//
+//  FactsView.swift
+//  CatFacts
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 05/02/21.
+//
+
+import Foundation
