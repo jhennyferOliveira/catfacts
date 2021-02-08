72cc72a0ce9c6cef6f975b0b9c9fdc4622290ac5
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-02-08 09:42:43 -0300

    creating collection view controller

--- CatFacts/CatFacts/View/FactCell/FactCollectionViewController.swift
+++ CatFacts/CatFacts/View/FactCell/FactCollectionViewController.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import UIKit
+
+class FactCollectionViewController: UIViewController, UICollectionViewDelegate {
+    
+    override func loadView() {
+        let factView = FactsView()
+        factView.card.delegate = self
+        factView.card.dataSource = self
+        factView.controller = self
+        self.view = factView
+        
+        
+
+       }
+
+       override func viewDidLoad() {
+           super.viewDidLoad()
+
+       }
+    
+    
+    
+    
+    
+    
+}
+
+extension FactCollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FactCell.identifier, for: indexPath)
+        if let cell = cell as? FactCell {
+            // get from API
+            cell.cardLabel.text = "Polydactyl cats (a cat with 1-2 extra toes on their paws) have this as a result of a genetic mutation. These cats are also referred to as 'Hemingway cats' because writer Ernest Hemingway reportedly owned dozens of them at his home in Key West, Florida."
+        }
+        
+        return cell
+    }
+    
+    
+}
