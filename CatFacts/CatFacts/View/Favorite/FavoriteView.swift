c46be64fcffc1ba4bf04adb60d04d902523413d5
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-02-08 09:42:09 -0300

    creating favorite view and its controller

--- CatFacts/CatFacts/View/Favorite/FavoriteView.swift
+++ CatFacts/CatFacts/View/Favorite/FavoriteView.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import UIKit
+
+class FavoriteView: UIView {
+    var controller : FavoriteViewController?
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
+        collectionView.register(FactCell.self, forCellWithReuseIdentifier: FactCell.identifier)
+        collectionView.backgroundColor = .yellowPrimary
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        return collectionView
+    }()
+    
+    func setUpConstraints() {
+        self.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpConstraints()
+        self.backgroundColor = .yellowPrimary
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension FavoriteView {
+    
+    private func collectionViewLayout() -> UICollectionViewLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 16
+        flowLayout.scrollDirection = .vertical
+        flowLayout.sectionInset = UIEdgeInsets(
+            top: 46,
+            left: 0,
+            bottom: 16,
+            right: 0)
+        flowLayout.itemSize = CGSize(width: 336, height: 242)
+        return flowLayout
+    }
+    
+}
--- CatFacts/CatFacts/View/Favorite/FavoriteViewController.swift
+++ CatFacts/CatFacts/View/Favorite/FavoriteViewController.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+class FavoriteViewController: UIViewController, UICollectionViewDelegate {
+    override func loadView() {
+        let favoriteView = FavoriteView()
+        favoriteView.collectionView.delegate = self
+        favoriteView.collectionView.dataSource = self
+        favoriteView.controller = self
+        self.view = favoriteView
+       }
+}
+
+extension FavoriteViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FactCell.identifier, for: indexPath)
+        if let cell = cell as? FactCell {
+            // get from API
+            cell.cardLabel.text = "Polydactyl cats (a cat with 1-2 extra toes on their paws) have this as a result of a genetic mutation. These cats are also referred to as 'Hemingway cats' because writer Ernest Hemingway reportedly owned dozens of them at his home in Key West, Florida."
+        }
+        
+        return cell
+    
+    }
+    
+    
+}
