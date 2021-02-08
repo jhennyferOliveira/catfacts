81d9e966849dd1e852221856cf5a78a5bfd049a0
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-02-08 09:40:17 -0300

    configuring app and scene delegate do run view code

--- CatFacts/CatFacts/Config/SceneDelegate.swift
+++ CatFacts/CatFacts/Config/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-//        window?.rootViewController = 
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
      
     }
 
