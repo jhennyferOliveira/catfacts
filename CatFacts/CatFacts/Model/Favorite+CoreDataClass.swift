cfe8c839ee956792da822ce172c5f43f84442413
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-02-09 09:26:26 -0300

    adding Core Data Entities

---
+++ CatFacts/CatFacts/Model/Favorite+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Favorite+CoreDataClass.swift
+//  CatFacts
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 09/02/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Favorite)
+public class Favorite: NSManagedObject {
+
+}
---
+++ CatFacts/CatFacts/Service/APIHandler.swift
@@ -0,0 +1,8 @@
+//
+//  APIHandler.swift
+//  CatFacts
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 08/02/21.
+//
+
+import Foundation
