3e000ecf1b1bdb6cf6b76bfc7ce3204d53b56ae2
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-02-09 09:30:37 -0300

    getting data from view model and passing to the view

--- CatFacts/CatFacts/View/FactCell/FactCollectionViewController.swift
+++ CatFacts/CatFacts/View/FactCell/FactCollectionViewController.swift
@@ -10,39 +10,52 @@ import UIKit
 
 class FactCollectionViewController: UIViewController, UICollectionViewDelegate {
     
+    private let myViewModel = ViewModel()
+    var button: UIButton?
+    var collectionView: UICollectionView?
+
+    
     override func loadView() {
         let factView = FactsView()
         factView.card.delegate = self
         factView.card.dataSource = self
         factView.controller = self
+        self.collectionView = factView.card
+        self.button = factView.buttonNewFact
+        button?.addTarget(self, action: #selector(newFactButton), for: .touchUpInside)
         self.view = factView
+        getData()
         
-        
-
-       }
-
-       override func viewDidLoad() {
-           super.viewDidLoad()
-
-       }
+    }
     
+    @objc func newFactButton() {
+        getData()
+    }
     
-    
-    
-    
-    
+    func getData() {
+        myViewModel.getFact { fact in
+            DispatchQueue.main.async {
+                self.myViewModel.fact = fact
+                // reload when fact gets the value
+                self.collectionView?.reloadData()
+            }
+        }
+    }
 }
+
+
 
 extension FactCollectionViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       return 1
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FactCell.identifier, for: indexPath)
+        
         if let cell = cell as? FactCell {
-            // get from API
-            cell.cardLabel.text = "Polydactyl cats (a cat with 1-2 extra toes on their paws) have this as a result of a genetic mutation. These cats are also referred to as 'Hemingway cats' because writer Ernest Hemingway reportedly owned dozens of them at his home in Key West, Florida."
+            cell.cardLabel.text = myViewModel.fact?.fact
         }
         
         return cell
