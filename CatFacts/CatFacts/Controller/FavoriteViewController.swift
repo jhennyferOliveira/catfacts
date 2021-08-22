461cb94cb1271bdab7ac926d9dac6de483f00e78
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2021-08-22 17:01:37 -0300

    renaming vars and functions

--- CatFacts/CatFacts/Controller/FavoriteViewController.swift
+++ CatFacts/CatFacts/Controller/FavoriteViewController.swift
@@ -12,12 +12,13 @@ class FavoriteViewController: UIViewController, UICollectionViewDelegate {
     
     let viewModelFavorite = ViewModelFavorite.sharedInstance
     var collectionView: UICollectionView?
-    var placeholder: UILabel?
-    let impact = UIImpactFeedbackGenerator()
+    var placeholderForEmptyScreen: UILabel?
+    let impactFeedbackForFavoriteButton = UIImpactFeedbackGenerator()
     
     override func viewWillAppear(_ animated: Bool) {
-        getData()
+        getFavoritedFacts()
     }
+    
     override func loadView() {
         super.loadView()
         let favoriteView = FavoriteView()
@@ -25,33 +26,29 @@ class FavoriteViewController: UIViewController, UICollectionViewDelegate {
         favoriteView.collectionView.dataSource = self
         favoriteView.controller = self
         collectionView = favoriteView.collectionView
-        placeholder = favoriteView.placeholderNoFavoritedFacts
+        placeholderForEmptyScreen = favoriteView.placeholderNoFavoritedFacts
         self.view = favoriteView
         overrideUserInterfaceStyle = .light
-        
     }
     
-    func getData() {
+    func getFavoritedFacts() {
         viewModelFavorite.favoritedFacts = viewModelFavorite.getAll()
         self.collectionView?.reloadData()
     }
 }
 
 extension FavoriteViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if viewModelFavorite.favoritedFacts?.count == 0 {
-            placeholder?.isHidden = false
+            placeholderForEmptyScreen?.isHidden = false
         } else {
-            placeholder?.isHidden = true
+            placeholderForEmptyScreen?.isHidden = true
         }
         return viewModelFavorite.favoritedFacts?.count ?? 0
-        
     }
     
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      
-        
         guard let factCell  = collectionView.dequeueReusableCell(withReuseIdentifier: FactCell.identifier, for: indexPath) as? FactCell else { fatalError() }
         
         factCell.delegateFavoriteController = self
@@ -59,22 +56,19 @@ extension FavoriteViewController: UICollectionViewDataSource {
         factCell.favorite.setImage(UIImage(named: "heartFill"), for: .normal)
         factCell.favorite.tag = indexPath.row
         return factCell
-        
     }
 }
 
 extension FavoriteViewController: FavoriteButtonActionDelegateToFavoriteController {
 
     func removeFavoritedFactFromFavoriteView(button: UIButton) {
-        impact.impactOccurred()
+        impactFeedbackForFavoriteButton.impactOccurred()
         viewModelFavorite.currentFactIsFavorite = !viewModelFavorite.currentFactIsFavorite
         let favoriteFacts = viewModelFavorite.favoritedFacts
-        guard let id = favoriteFacts?[button.tag].id else {return}
-        viewModelFavorite.deleteItem(id:id)
-        getData()
+        guard let idFavoritedFactToBeRemoved = favoriteFacts?[button.tag].id else {return}
+        viewModelFavorite.deleteItem(id:idFavoritedFactToBeRemoved)
+        getFavoritedFacts()
         collectionView?.reloadData()
-             
- 
     }
 }
 
